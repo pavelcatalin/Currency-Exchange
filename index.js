2cc9d32b887b1696f3f31c2793913501bf011ec6
Pavel Catalin <pavelcc11@yahoo.com>
2021-10-18 01:13:06 +0300

    In progress

--- index.js
+++ index.js
@@ -1,127 +1,33 @@
- async function getData (value) {
+async function getData (value) {
     
     let fetchApi = await fetch(`http://api.exchangeratesapi.io/v1/${value}?access_key=760b5f60702c73714c5ed590ceaa403b`);
     let response = await fetchApi.json(); 
-   
     
-    return response
+    return  response
 };
-//global variables
 
-let listOfCurrencies = document.querySelector('.currencies-list');
+document.querySelector('.open-modal').addEventListener('click', setListCurrencies)
 
-//
-let baseCurrencies = [];
+async function setListCurrencies (){
+    let data = await getData('symbols');
+    const listOfCurrencies = document.querySelector('.currencies-list');
 
-
-let currenciesData = {
-    
-}
-
-
-
-async function populateCurrencyList(){
-    
-    let abreviation = await getData("symbols")
-    
-    for(let prop in  abreviation.symbols){
-       
-        listOfCurrencies.insertAdjacentHTML('beforeend',
-        `
-        <li id=${prop}>
-            <img src="Assets/flags/${prop}.png" >
-            <span>${prop} </span><span>  - ${ abreviation.symbols[prop]}</span>
-        </li>
-        `
+    Object.keys(data.symbols).forEach(currency =>
+        
+         listOfCurrencies.insertAdjacentHTML('beforeend',`
+            <li id=${currency}>
+            <img src=Assets/flags/${currency}.png>
+            <span>${currency}</span>
+            <span>${data.symbols[currency]}</span>
+            </li>
+        
+        `)
         )
-    }
+
+    //console.log();
+
 }
 
-populateCurrencyList()
-
-
-listOfCurrencies.addEventListener('click', selectedBaseCurrencies)
-
-function selectedBaseCurrencies(e){
-   let selectedCurrency = e.target.closest('li');
-   let currencyAbreviation = selectedCurrency.getAttribute('id');
-   
-   if( !selectedCurrency.classList.contains('disabled')){
-        selectedCurrency.classList.add('disabled')
-        createCard(currencyAbreviation)
-    }
-}
-
-function createCard(currencyAbreviation){
-    let cardContainer = document.querySelector('.card-container');
-    let amount = document.querySelector('#amount').value
-    cardContainer.insertAdjacentHTML('beforeEnd',`
-    
-    <div class="card" id=${currencyAbreviation}>
-
-                <div class="close-card">
-                    <button class="close-card-btn">X</button>
-                </div>
-
-                <div class="flag-amount">
-                    <img src="Assets/flags/${currencyAbreviation}.png" alt="">
-                    <span class="symbol">&euro;</span>
-                    <span class="amount">${amount}</span>
-                </div>
-
-                <div class="name-rate">
-                    <p class="abreviation"><span>EUR</span>-<span>Euro</span></p>
-                    <p class="rate"><span>EUR</span>=<span>EUR</span></p>
-                </div>
-
-            </div>
-    
-    `)
-}
-
-document.querySelector('.card-container').addEventListener('click', closeCard);
-
-function closeCard(e){
-    if(e.target.classList.contains('close-card-btn')){
-       let parentelement = e.target.parentElement.parentElement;
-       let parentElementId = parentelement.getAttribute('id')
-       parentelement.style.display = "none";
-        document.querySelector(`li#${parentElementId}`).classList.remove('disabled')
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let displayModal = () => document.querySelector('.modal-container').style.display = 'flex'  
-
-
-document.querySelector('.open-modal').addEventListener('click', displayModal)
-
-let closemodal = () => document.querySelector('.modal-container').style.display = 'none'
-
-document.querySelector('.modal-container').addEventListener('click', (e)=>{
-    let modal = document.querySelector('.modal-container')
-    if(e.target == modal){
-        closemodal()
-    }
+document.querySelector('.open-modal').addEventListener('click', ()=>{
+    document.querySelector('.modal-container').style.display = "flex"
 })
